bdbc893c1740559160b59b93c52675eb889ac3fc
unknown <mochammad_a_rozali@telkomsel.co.id>
2024-08-24 11:27:27 +0700

    fixing apps.js untuk express.json()

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(require("./routes"));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+app.use("/",require("./routes"));
 
 //error handling
 app.use((err, req, res, next) => {
--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -3,7 +3,10 @@ const { User } = require("../models");
 exports.register = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json(user);
+    res.status(201).json({
+      message: "Success creating new user",
+      user,
+    });
   } catch (error) {
     next(error);
   }
