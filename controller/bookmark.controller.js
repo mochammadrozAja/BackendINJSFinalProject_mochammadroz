215e5745439a6441df8864b7d094d65bd4441abb
unknown <mochammad_a_rozali@telkomsel.co.id>
2024-08-28 22:57:48 +0700

    final project fixing bookmark controller

--- controller/bookmark.controller.js
+++ controller/bookmark.controller.js
@@ -1,17 +1,20 @@
 const { Bookmark, Movie, User } = require("../models");
 
+const getUserIdFromToken = (token) => {
+  try {
+    return JSON.parse(atob(token.split(" ")[1].split(".")[1])).id;
+  } catch (error) {
+    throw new Error("Invalid token");
+  }
+};
+
 exports.index = async (req, res, next) => {
-  const reqUserId = JSON.parse(atob(req.headers.authorization.split(" ")[1].split(".")[1])).id;
+  const reqUserId = getUserIdFromToken(req.headers.authorization);
 
   const options = {
     include: ["movie"],
+    where: reqUserId ? { userId: reqUserId } : {},
   };
-
-  if (reqUserId) {
-    options.where = {
-      userId: reqUserId,
-    };
-  }
 
   try {
     const bookmarks = await Bookmark.findAll(options);
@@ -23,40 +26,38 @@ exports.index = async (req, res, next) => {
 
 exports.create = async (req, res, next) => {
   const { id } = req.params;
-  const reqUserId = JSON.parse(atob(req.headers.authorization.split(" ")[1].split(".")[1])).id;
-  
+  const reqUserId = getUserIdFromToken(req.headers.authorization);
+
   try {
-    const movie = await Movie.findOne({
-      where: { id },
+    const movie = await Movie.findOne({ where: { id } });
+
+    if (!movie) {
+      return res.status(404).json({ message: "Movie not found" });
+    }
+
+    const [bookmark, created] = await Bookmark.findOrCreate({
+      where: { movieId: id, userId: reqUserId },
+      defaults: { movieId: id, userId: reqUserId },
     });
 
-    const bookmark = await Bookmark.create({movieId: id,userId: reqUserId});
-    res.status(201).json({message: "success adding new bookmark",
-      id:bookmark.id,userId:bookmark.userId,movieId:bookmark.movieId,movie:movie.title
-    });
-  } catch (error) {
-    if(error.message=="Validation error") {
-      const movie = await Movie.findOne({
-        where: { id },
-      });
-      
-      const options = {
-        include: ["movie"],
-      };
-    
-      if (reqUserId) {
-        options.where = {
-          userId: reqUserId,
-        };
-      }
-
-      const bookmarks = await Bookmark.findOne(options);
+    if (!created) {
       return res.status(200).json({
-        message: 'bookmark already exists',
-        id:bookmarks.id,userId:bookmarks.userId,movieId:bookmarks.movieId,movie:movie.title
+        message: "Bookmark already exists",
+        id: bookmark.id,
+        userId: bookmark.userId,
+        movieId: bookmark.movieId,
+        movie: movie.title,
       });
     }
+
+    res.status(201).json({
+      message: "Success adding new bookmark",
+      id: bookmark.id,
+      userId: bookmark.userId,
+      movieId: bookmark.movieId,
+      movie: movie.title,
+    });
+  } catch (error) {
     next(error);
-    // console.log(error.message,"ardian error");
   }
 };
